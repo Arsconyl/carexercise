fb3556432686265a9aac0460eb82fa77e0d3b171
Arnaud Couderc <costar57@gmail.com>
2022-04-25 18:25:30 +0200

    :tada: First commit

---
+++ src/main/java/com/ptc/carexercise/CarexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ptc.carexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarexerciseApplication.class, args);
+	}
+
+}
